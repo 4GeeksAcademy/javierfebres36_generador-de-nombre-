d1c884ee9638d88ed9a712878a09b475d5cef7b4
javierfebres36 <164325528+javierfebres36@users.noreply.github.com>
2024-06-08 21:41:42 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "an", "some", "any"];
+  let adj = ["great", "big", "awesome", "cool", "funny"];
+  let noun = ["jogger", "racoon", "webside", "appside", "blog"];
+  let tlds = [".org", ".com", ".net", ".space", ".de", ".cat", ".dog"];
+
+  for (let pronoun of pronoun) {
+    for (let adj of adj) {
+      for (let noun of noun) {
+        for (let tlds of tlds) {
+          console.log(`${pronoun}${adj}${noun}${tlds}`);
+        }
+      }
+    }
+  }
 };
